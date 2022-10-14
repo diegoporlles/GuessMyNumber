3135967abdfe7707f0ff41c4d5a5b87afb992c88
diegoporlles <diego.porlles.22@gmail.com>
2022-10-13 20:37:36 -0500

    First

---
+++ script.js
@@ -0,0 +1,63 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+const styleBody = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+const styleNumber = function (number) {
+  document.querySelector('.number').style.width = number;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No Number!');
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    displayNumber(secretNumber);
+    styleBody('#60b347');
+    styleNumber('30rem');
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('💥 You lost the game');
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayScore(score);
+  displayNumber('?');
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  styleBody('#222');
+  styleNumber('15rem');
+});
